1369b0908a9d93cca383fbd4e52e8ed668c31ed5
Ali Shan <alishan8543402@gmail.com>
2025-10-15 09:52:48 +0500

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">🤖 AI Tool Hub</div>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/tools">Tools</a>
+        <a href="/privacy">Privacy</a>
+      </div>
+    </nav>
+  );
+}
